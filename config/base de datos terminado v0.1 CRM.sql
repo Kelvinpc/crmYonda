f6ff8796c0ba72f0d009a89395a6768beda0e3cd
kelvin <kelvinpc444@gmail.com>
2025-04-29 12:14:48 -0500

    falta terminar sql

--- config/base de datos terminado v0.1 CRM.sql
+++ config/base de datos terminado v0.1 CRM.sql
@@ -5,32 +5,35 @@ USE crmYonda;
 
 CREATE TABLE roles(
 
-	id_rol		INT AUTO_INCREMENT PRIMARY KEY,
-    rol			ENUM ('Jefe de Marketing','Asesor')
+	idrol		INT AUTO_INCREMENT PRIMARY KEY,
+    rol			VARCHAR(30)
 
 ) ENGINE = INNODB;
 
-INSERT INTO ROLES (rol) VALUES('Jefe de Marketing'),('Asesor');
-SELECT * FROM ROLES;
+INSERT INTO roles (rol) VALUES('Jefe de Marketing'),('Asesor');
+SELECT * FROM roles;
 
-CREATE TABLE ESTADOS(
+CREATE TABLE estados(
 	
-    id_estado	INT AUTO_INCREMENT PRIMARY KEY,
-    indice		INT UNIQUE,
-    estado		ENUM('leads','MQL','SQL') 		NOT NULL
+    idestado	INT AUTO_INCREMENT PRIMARY KEY,
+    indice		INT,
+    estado		VARCHAR(30)NOT NULL
+
+    CONSTRAINT uk_indice_estados UNIQUE (indice)
     
 )ENGINE = INNODB;
 
-INSERT INTO ESTADOS (indice,estado)VALUES
+INSERT INTO estados (indice,estado)VALUES
 					(1,'leads'),
 					(2,'MQL'),
 					(3,'SQL');
-SELECT * FROM ESTADOS;
+SELECT * FROM estados;
 
 CREATE TABLE origenes(
 
-	id_origen	INT AUTO_INCREMENT PRIMARY KEY,
-    origen		VARCHAR(30) UNIQUE
+	idorigen	INT AUTO_INCREMENT PRIMARY KEY,
+    origen		VARCHAR(30),
+    CONSTRAINT uk_origen_origenes UNIQUE (origen)
 
 )ENGINE = INNODB;
 
@@ -46,36 +49,35 @@ SELECT * FROM origenes;
 
 CREATE TABLE personas(
 	
-	id_persona		INT AUTO_INCREMENT PRIMARY KEY,
-    apellido_pat	VARCHAR(30),
-    apellido_mat	VARCHAR(30),
+	idpersona		INT AUTO_INCREMENT PRIMARY KEY,
+    apellidos       VARCHAR(50),
     nombres			VARCHAR(30),
-    tipodoc			ENUM('DNI','Carnet de extranjería'),
-    numdoc			VARCHAR(12) UNIQUE,
+    tipodoc			ENUM('DNI','CEX','PASS') COMMENT 'CEX = Carnet de EXtrangeria ; PASS=Pasaporte',
+    numdoc			VARCHAR(12),
     fechanac		DATE,
-    telefono		VARCHAR(16),
+    telefono		VARCHAR(12),
     email			VARCHAR(255),
-    id_origen		INT NULL,
+    idorigen		INT NULL,
     
-    FOREIGN KEY (id_origen) REFERENCES origenes(id_origen)
+    CONSTRAINT fk_idorigen_origenes FOREIGN KEY (idorigen) REFERENCES origenes(idorigen)
     
 )ENGINE = INNODB;
 
 
-INSERT INTO personas (apellido_pat,apellido_mat,nombres,tipodoc,numdoc,fechanac,telefono,email,id_origen)values
-('Pérez', 'González', 'Juan Carlos', 'DNI', '12345678', '1985-06-15', '987654321', 'juan.perez@example.com', NULL),
-('Sánchez', 'Morales', 'Ana María', 'Carnet de extranjería', 'A1234567', '1990-09-10', '945678123', 'ana.sanchez@example.com', NULL),
-('López', 'Ramírez', 'Carlos Alberto', 'DNI', '87654321', '1995-12-01', '912345678', 'carlos.lopez@example.com', NULL),
+INSERT INTO personas (apellidos,nombres,tipodoc,numdoc,fechanac,telefono,email,idorigen)values
+('Pérez González', 'Juan Carlos', 'DNI', '12345678', '1985-06-15', '987654321', 'juan.perez@example.com', NULL),
+('Sánchez Morales', 'Ana María', 'Carnet de extranjería', 'A1234567', '1990-09-10', '945678123', 'ana.sanchez@example.com', NULL),
+('López Ramírez', 'Carlos Alberto', 'DNI', '87654321', '1995-12-01', '912345678', 'carlos.lopez@example.com', NULL),
 
-('Gómez', 'Martínez', 'Luisa Fernanda', 'Carnet de extranjería', 'B2345678', '1982-03-25', '965432109', 'luisa.gomez@example.com', 3),
-('Martínez', 'Suárez', 'Ricardo', 'DNI', '23456789', '1992-11-20', '930123456', 'ricardo.martinez@example.com', 1),
-('Vásquez', 'Luna', 'Elena', 'Carnet de extranjería', 'C3456789', '1988-04-12', '928765432', 'elena.vasquez@example.com', 1),
+('Gómez Martínez', 'Luisa Fernanda', 'Carnet de extranjería', 'B2345678', '1982-03-25', '965432109', 'luisa.gomez@example.com', 3),
+('Martínez Suárez', 'Ricardo', 'DNI', '23456789', '1992-11-20', '930123456', 'ricardo.martinez@example.com', 1),
+('Vásquez Luna', 'Elena', 'Carnet de extranjería', 'C3456789', '1988-04-12', '928765432', 'elena.vasquez@example.com', 1),
 
-('Pipa', 'Castilla', 'Kelvin', 'DNI', '74747474', '2005-08-29', '955959955', 'kelvin@gmail.com', NULL);
+('Pipa Castilla', 'Kelvin', 'DNI', '74747474', '2005-08-29', '955959955', 'kelvin@gmail.com', NULL);
 
-INSERT INTO personas (apellido_pat,apellido_mat,nombres,tipodoc,numdoc,fechanac,telefono,email,id_origen)values
-('Fernández', 'Torres', 'Marta Beatriz', 'DNI', '134345678', '1990-05-12', '982345678', 'marta.fernandez@example.com', 1),
-('Castillo', 'Ríos', 'José Luis', 'Carnet de extranjería', 'C9876543', '1985-02-20', '961234567', 'jose.castillo@example.com', 2);
+INSERT INTO personas (apellidos,nombres,tipodoc,numdoc,fechanac,telefono,email,idorigen)values
+('Fernández Torres', 'Marta Beatriz', 'DNI', '134345678', '1990-05-12', '982345678', 'marta.fernandez@example.com', 1),
+('Castillo Ríos', 'José Luis', 'Carnet de extranjería', 'C9876543', '1985-02-20', '961234567', 'jose.castillo@example.com', 2);
 SELECT * FROM personas;
 
 
@@ -84,17 +86,18 @@ SELECT * FROM personas;
 
 CREATE TABLE contratos(
 
-	id_contrato		INT AUTO_INCREMENT PRIMARY KEY,
-    id_persona		INT,
+	idcontrato		INT AUTO_INCREMENT PRIMARY KEY,
+    idpersona		INT,
     fechainicio		DATE,
     fechafin		DATE,
-    id_rol			INT,
-    FOREIGN KEY (id_rol) REFERENCES ROLES(id_rol),
-	FOREIGN KEY (id_persona) REFERENCES personas(id_persona)
+    idrol			INT,
+    CONSTRAINT fk_idrol_roles FOREIGN KEY (idrol) REFERENCES roles(idrol),
+	CONSTRAINT fk_idpersona_personas FOREIGN KEY (idpersona) REFERENCES personas(idpersona)
+    
 
 )ENGINE = INNODB;
 
-INSERT INTO contratos (id_persona, fechainicio, fechafin, id_rol)
+INSERT INTO contratos (idpersona, fechainicio, fechafin, idrol)
 VALUES
 (1, '2025-01-01', '2025-06-28', 2),
 (2, '2025-01-15', '2025-06-14', 2),
@@ -113,11 +116,11 @@ CREATE TABLE usuarios(
     nomuser			VARCHAR(30) UNIQUE,
     passuser		VARCHAR(255),
     
-    FOREIGN KEY (id_contrato) REFERENCES contratos(id_contrato)
+    CONSTRAINT fk_idcontrato_usuarios FOREIGN KEY (id_contrato) REFERENCES contratos(id_contrato)
 
 )ENGINE = INNODB;
 
-INSERT INTO usuarios(id_contrato,nomuser,passuser)VALUES
+INSERT INTO usuarios(idcontrato,nomuser,passuser)VALUES
 (1,'Juan','9IJN0OKM'),
 (2,'Ana','8UHB7YGV'),
 (3,'Carlos','6TFC5RDX'),
@@ -126,19 +129,19 @@ INSERT INTO usuarios(id_contrato,nomuser,passuser)VALUES
 SELECT * FROM usuarios;
 
 CREATE TABLE asignaciones(
-	id_asignaciones		INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuarioasigna	INT,
-	id_usuarioasesor	INT,
+	idasignaciones		INT AUTO_INCREMENT PRIMARY KEY,
+    idusuarioasigna	INT,
+	idusuarioasesor	INT,
     
-	FOREIGN KEY (id_usuarioasigna) REFERENCES usuarios(id_usuario), -- NOMBRE DEL LA PERSONA QUE ASIGNO
-	FOREIGN KEY (id_usuarioasesor) REFERENCES usuarios(id_usuario) -- NOMBRE DEL ASCESOR
+	CONSTRAINT fk_idusuarioasigna_usuarios FOREIGN KEY (idusuarioasigna) REFERENCES usuarios(idusuario), -- NOMBRE DEL LA PERSONA QUE ASIGNO
+	CONSTRAINT fk_idusuarioasesor_usuarios FOREIGN KEY (idusuarioasesor) REFERENCES usuarios(idusuario) -- NOMBRE DEL ASCESOR
 )ENGINE =INNODB;
 
 
 
 
 
-INSERT INTO asignaciones(id_usuarioasigna,id_usuarioasesor)
+INSERT INTO asignaciones(idusuarioasigna,idusuarioasesor)
 VALUES
 (4,1),
 (4,2),
@@ -150,17 +153,17 @@ SELECT * FROM asignaciones;
 
 CREATE TABLE carga(
 
-	id_carga	INT AUTO_INCREMENT PRIMARY KEY,
-    id_asignaciones	INT,
-    id_persona	INT,
+	idcarga	INT AUTO_INCREMENT PRIMARY KEY,
+    idasignaciones	INT,
+    idpersona	INT,
     
-	FOREIGN KEY (id_asignaciones) REFERENCES asignaciones(id_asignaciones),
-	FOREIGN KEY (id_persona) REFERENCES personas(id_persona)
+	CONSTRAINT fk_idasignaciones_asignaciones FOREIGN KEY (idasignaciones) REFERENCES asignaciones(idasignaciones),
+	CONSTRAINT fk_idpersona_personas FOREIGN KEY (idpersona) REFERENCES personas(idpersona)
     
 )ENGINE = INNODB;   
 
 
-INSERT INTO carga(id_asignaciones,id_persona)
+INSERT INTO carga(idasignaciones,idpersona)
 VALUES
 (1,4),
 (2,5),
@@ -173,19 +176,19 @@ SELECT * FROM carga;
 
 CREATE TABLE seguimiento(
 
-	id_seguimiento	INT AUTO_INCREMENT PRIMARY KEY,
+	idseguimiento	INT AUTO_INCREMENT PRIMARY KEY,
     fechainicio		DATE,
     fechafin		DATE,
-    id_estado		INT,
-    id_carga		INT,
-	FOREIGN KEY (id_estado) REFERENCES estados(id_estado),
-	FOREIGN KEY (id_carga) REFERENCES carga(id_carga)
+    idestado		INT,
+    idcarga		INT,
+	FOREIGN KEY (idestado) REFERENCES estados(idestado),
+	FOREIGN KEY (idcarga) REFERENCES carga(idcarga)
 )ENGINE = INNODB;
 
 
 
 
-INSERT INTO seguimiento(fechainicio,fechafin,id_estado,id_carga)
+INSERT INTO seguimiento(fechainicio,fechafin,idestado,idcarga)
 VALUES
 ('2025-01-10','2025-03-24',1,1),
 ('2025-02-14','2025-02-21',1,2),
