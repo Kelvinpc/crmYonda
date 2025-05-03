8a5b70bb74f522f8968f4c197a3a1bf0b5ed2cef
kelvin <kelvinpc444@gmail.com>
2025-05-03 13:03:07 -0500

    vista

--- routes/personas.js
+++ routes/personas.js
@@ -7,4 +7,29 @@ router.get('/', (req, res) =>{
 
 })
 
+render.get('/', async (req,res) =>{
+    try{
+
+        const query =`SELECT 
+        P.apellidos,
+        P.nombres,
+        P.tipodoc,
+        P.numdoc,
+        P.fechanac,
+        P.telefono,
+        P.email,
+        P.idorigen,
+        P.fechacreacion,
+        P.fechamodificado
+        FROM personas P 
+        INNER JOIN 
+        ;`
+        const [datosPersona]
+        
+    }catch(error){
+        console.error(error);
+        
+    }
+})
+
 module.exports = router;
